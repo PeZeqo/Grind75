44ce7fe4d6abaeab118dc97929186e792ca4e4f5
Peter Zeqo <peterzeqo@gmail.com>
2025-05-05 16:58:56 -0400

    Change filenames to CapitalizedCase. Adding question 7

---
+++ BinaryTree/BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,34 @@
+"""
+Solution:
+Runtime 100% O(n)
+Memory 18% O(n)
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        queue = deque([root])
+        res = []
+
+        while queue:
+            row = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                row.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(row)
+
+        return res
